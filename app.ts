24faaac7a79c817e4167d64518be1cf684c870b0
alihassan2345 <160609826+alihassan2345@users.noreply.github.com>
2024-03-07 07:18:45 +0500

    Add files via upload

--- app.ts
+++ app.ts
@@ -1,4 +1,2 @@
-let qoute: string= "A person who never made a mistake never tried anything new";
-let famous_person="Albert Einstein";
-let message=` ${famous_person} once said, ${qoute}`;
-console.log(message);
+let personName: string="ERIC";
+console.log(`hellow ${personName}, would you like to learn some python today?`);
